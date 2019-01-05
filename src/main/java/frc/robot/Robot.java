7b56c2c92fc0f2713f83a6bb29dd5a88b4ed4fd1
kevok <kevok93@gmail.com>
2019-01-05 13:22:50 -0500

    Initial commit 2019

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,64 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.*;
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.*;
+import com.kauailabs.navx.frc.*;
+
+
+
+public class Robot extends TimedRobot {
+  TalonSRX testTalon;
+  AHRS navx;
+
+
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+
+
+}
