ef6258aab63febe4909d27147ae1ff1b46d8253e
NeelS <nsortur@yahoo.com>
2019-02-10 16:28:23 -0500

    Working server and client connection

--- src/main/java/frc/robot/vision/VisionStandalone.java
+++ src/main/java/frc/robot/vision/VisionStandalone.java
@@ -1,16 +1,49 @@
 package frc.robot.vision;
 
-import java.util.Scanner;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class VisionStandalone {
-    public static void main(String... argv) {
-        VisionListener listener = new VisionListener();
-        listener.start();
+    private static DatagramSocket socket;
+    private static byte[] buf = new byte[256];
 
-        Scanner scan = new Scanner(System.in);
-        scan.next();
+    public static void main(String... argv) throws Exception {
 
-        listener.die = true;
+        socket = new DatagramSocket(5801);
+        run();
+//        VisionListener listener = new VisionListener();
+//        listener.start();
+//
+//        Scanner scan = new Scanner(System.in);
+//        scan.next();
+//
+//        listener.die = true;
 
+    }
+
+    private static void run() throws Exception {
+        boolean running = true;
+
+        while (running) {
+            DatagramPacket packet
+                    = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+            packet = new DatagramPacket(buf, buf.length, address, port);
+            String received
+                    = new String(packet.getData(), 0, packet.getLength());
+
+            System.out.println(received);
+
+            if (received.equals("end")) {
+                running = false;
+                continue;
+            }
+            socket.send(packet);
+        }
+        socket.close();
     }
 }
