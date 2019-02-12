726ec5b26fe45ffa69aeb4864f8b37cbd5c429bb
Zyansheep <zykersheep@gmail.com>
2019-02-12 18:06:37 -0500

    Prints to smart dashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import io.github.pseudoresonance.pixy2api.Pixy2;
 
 import io.github.pseudoresonance.pixy2api.Pixy2Line;
 import io.github.pseudoresonance.pixy2api.links.SPILink;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 
@@ -61,6 +62,12 @@ public class Robot extends TimedRobot {
         Pixy2Line.Vector[] vec = pixyCam.getLine().getVectors();
 
         if(vec == null){ return; }
+        //Print vector coords to smartdashboard
+        SmartDashboard.putNumber(vec[0].getX0());
+        SmartDashboard.putNumber(vec[0].getX1());
+        SmartDashboard.putNumber(vec[0].getY0());
+        SmartDashboard.putNumber(vec[0].getX1());
+
         for(int i=0;i<vec.length;i++){
             System.out.println(vec[i].toString());
         }
