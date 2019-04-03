ca2898c72b7acfa4835092ad545fd415def3f0e8
audrey-yang <ayang24.robin@gmail.com>
2019-04-02 21:02:28 -0400

    Pre-Lehigh: Note to self to check the shifter on MOEva.

--- src/main/java/frc/robot/genericrobot/GenericRobot.java
+++ src/main/java/frc/robot/genericrobot/GenericRobot.java
@@ -91,8 +91,8 @@ public abstract class GenericRobot {
 	protected abstract void setDrivePowerInternal(double leftMotor, double rightMotor);
 
 	//shifting
-	public void shiftLow () { shiftDrive(true); }
-	public void shiftHigh  () { shiftDrive(false); } //CHANGE THESE BACK BEFORE LEAVINGGGGGGGGGG
+	public void shiftLow () { shiftDrive(false); }
+	public void shiftHigh  () { shiftDrive(true); } //check on MOEva
 
 	public void shiftDrive(boolean state) {
 		this.shifterSolenoidState = state;
